c23ab64918f9b47e8ebdb92e6235002f97acb295
daisy <pliu@mozilla.com>
2019-08-21 15:11:20 +0800

    news point api and gradle task

---
+++ src/test/java/services/UserService.java
@@ -0,0 +1,10 @@
+package services;
+
+public class UserService {
+
+    // public class login()
+    // public class logout()
+    // public class delete account()
+
+}
+
---
+++ src/test/java/userServiceTest.java
@@ -0,0 +1,27 @@
+
+import org.junit.Test;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class userServiceTest {
+
+    /*
+        @Test
+        public void loginTest() {
+     */
+
+    /*
+       @Test
+       public void logoutTest(){}
+     */
+    /*
+       @Test
+       public void delete(){}
+     */
+
+}
